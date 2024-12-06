125d8fb3eaeacd94aeb5370062708453a55e9bdc
Symbuh <nicholasrsabadicci@gmail.com>
2024-12-05 20:19:58 -0800

    Added comments

--- mergeTwoLists.js
+++ mergeTwoLists.js
@@ -1,3 +1,13 @@
+/*
+    How the recursive solution works
+    1. As long as both lists aren't null 
+        - Find the lesser of our two current values 
+        - Invoke a recursive function call on the next of that value and return. 
+
+    2. When we get to the bottom of the call stack and one of the lists is null 
+        - We return the rest of the remaining characters which should now appear in order 
+*/
+
 var mergeTwoLists = function(list1, list2) {
     if (list1 !== null && list2 !== null) {
         if (list1.val < list2.val) {
