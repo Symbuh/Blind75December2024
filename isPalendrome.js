2ca07078299a09d7bbc67c7892d63d84a5924ace
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2024-12-07 21:49:02 -0800

    isPalendrome optimizations 5ms faster than 60% of submissions.

--- isPalendrome.js
+++ isPalendrome.js
@@ -1,8 +1,11 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 var isPalindrome = function(s) {
-    // First we need to escape all of the characters
-    s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+    s = s.replace(/[^a-zA-Z0-9]/g, "")
+    s = s.toLowerCase()
 
-    console.log(s)
     for (let i = 0; i < s.length/2; i++) {
         if (s[i] !== s[s.length - 1 - i]) {
             return false
