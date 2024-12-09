269a8083b6ccf91b4d608808d1e33547337ea157
Nicholas Sabadicci <nicholassabadicci@Nicholass-MacBook-Pro.local>
2024-12-08 18:22:46 -0800

    IsAnagram

---
+++ isAnagram.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+
+    let memo = {}
+
+    for (let i = 0; i < s.length; i++) {
+        if (memo[s[i]]) {
+            memo[s[i]]++
+        } else {
+            memo[s[i]] = 1
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (!memo[t[i]]) {
+            return false
+        } else if (memo[t[i]] === 0) {
+            return false
+        } else {
+            memo[t[i]]--
+        }
+    }
+    return true
+};
