42eb22509b56035b5ae5db8da37e69a518fd47e5
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2024-12-23 20:44:45 -0800

    1ms, Beats 88.58% of Submissions

--- balancedBinaryTree.js
+++ balancedBinaryTree.js
@@ -30,10 +30,3 @@ var isBalanced = function(root) {
     return height(root) !== -1
 };
 // 1ms, beats 88.58% of submissions
-/*
-    So basically this is the most insane soultion ever and nobody could ever hope to understand it and I think that's something that's pretty fucked 
-    of them to just toss in there this is a painfully rude solution. 
-
-    But its basically the exact same logic that I used for mine without some of the issues. Each time he goes down a level he returns one higher, it's 
-    just a very strange and confusing way of doing so. I hate recursion and I think I need a more detailed explanation
-*/
