6beb20bcfe6598b88c6168276ce8982056bc3976
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2025-02-17 17:53:56 -0800

    isBadVersion.js

---
+++ isBadVersion2.js
@@ -0,0 +1,28 @@
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 0, end = n
+        
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2)
+
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid - 1)) {
+                    return mid 
+                }
+
+                end = mid - 1
+            } else {
+                if (isBadVersion(mid + 1)) {
+                    return mid + 1
+                }
+
+                start = mid + 1
+            }
+        }
+
+    };
+};
