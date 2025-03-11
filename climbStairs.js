19c095c24ba9443bc15e2301f018cd251d92c982
Nicholas Sabadicci <nicholassabadicci@Nicholass-MacBook-Pro.local>
2025-03-11 01:48:58 -0700

    climbStairs.js

---
+++ climbStairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n <= 3) {
+        return n 
+    }
+
+    let secondPrevious = 2, previous = 3
+
+    for (let i = 4; i < n; i ++) {
+        let temp = previous
+        previous = secondPrevious + previous
+        secondPrevious = temp
+    }
+    
+    return secondPrevious + previous
+};
