bfa1c0eb5f34ac11f09dfa0f14f1cbabe2deced1
Nicholas Sabadicci <nicholassabadicci@Nicholass-MacBook-Pro.local>
2025-03-12 22:32:59 -0700

    MaxDepth of Linked List

---
+++ maxDepth.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) {
+        return 0
+    }
+
+    let maxDepth = 0
+    
+    let inner = (node, currDepth) => {
+        if (node?.left) {
+            inner(node.left, currDepth + 1)
+        }
+        if (node?.right) {
+            inner(node.right, currDepth + 1)
+        }
+
+        if (!node?.right && !node?.left) {
+            if(currDepth > maxDepth) {
+                maxDepth = currDepth
+            }
+            return
+        }
+    }
+
+    inner(root, 1) 
+    return maxDepth
+};
