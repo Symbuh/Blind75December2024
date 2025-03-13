db24e3bd3a4077626a8b68b45e1a388eaa4f5f48
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2025-03-12 19:25:15 -0700

    Third iteration of longest Palendrome

--- longestPalendrome.js
+++ longestPalendrome.js
@@ -1,26 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var longestPalindrome = function(s) {
     let memo = {}, oddFound = false, longest = 0
 
     for (let i = 0; i < s.length; i++) {
         if (memo[s[i]]) {
-            memo[s[i]].push(i)
+            memo[s[i]]++
         } else {
-            memo[s[i]] = [i]
+            memo[s[i]] = 1
         }
     }
 
     for (item in memo) {
-        if (memo[item].length % 2 === 0) {
-            longest = longest + memo[item].length
-        } else {
+       if (memo[item] % 2 === 0) {
+            longest = longest + memo[item]
+       } else {
             if (!oddFound) {
                 longest = longest + 1
                 oddFound = true
             }
-
-            longest = longest + memo[item].length - 1
-        }
+            longest = longest + memo[item] - 1
+       }
     }
 
     return longest
-};// 25 ms, faster than 25% of solutions
+};
