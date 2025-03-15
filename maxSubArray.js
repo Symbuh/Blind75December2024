21b28af25d15f3f256f071f5d0097f758104ea36
Nicholas Sabadicci <nicholassabadicci@Nicholass-MacBook-Pro.local>
2025-03-14 17:22:45 -0700

    MaxSubArray

---
+++ maxSubArray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    max = nums[0]
+    total = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        if (total < 0) {
+            total = 0
+        }
+
+        total = total + nums[i]
+        max = Math.max(max, total)
+    }
+    return max
+}
