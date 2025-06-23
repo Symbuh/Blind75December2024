382013aa7dda783c2d3e13be2dd66e120108260b
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2025-06-23 11:56:13 -0700

    coinChange progress, running into an issue while making more complex combinations.

--- coinChange.js
+++ coinChange.js
@@ -1,1 +1,25 @@
-// Coin Change (coin sum)
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    coins.sort((a, b) => a - b)
+    let count = 0
+
+    for (let i = coins.length - 1; i >= 0; i--) {
+        if (amount >= coins[i]) {
+            let temp = Math.floor(amount / coins[i])
+            console.log(`Round amount ${amount} coin ${coins[i]} result: ${temp}`)
+            count = count + temp
+            amount = amount - (coins[i] * temp)
+        }
+    }
+
+    console.log(`after loop: Count ${count} Amount ${amount}`)
+    if (amount !== 0) {
+        return -1
+    }
+
+    return count
+};
