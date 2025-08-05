1571cbdcab309178442e4cc28e73dabf63dafe22
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2025-08-05 14:39:57 -0700

    Removed DFS Array to significantly reduce runtime.

--- numIslands.js
+++ numIslands.js
@@ -3,23 +3,11 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    // Storing the visited Array is unneccessary
-    // We can use the original array and re-assign values to zero... 
-    // this is going to save significant runtime and I cant believe that I 
-    // didn't remember this solution from previous attempts at the problem
-    // I'm frusterated with myself and I'm spamming a bunch of typing for 
-    // extra commit swag
-    let visited = []
-    
-    for (let row of grid) {
-        let visRow = Array(row.length).fill(0)
-        visited.push(visRow)
-    }
 
     let markIsland = (x, y) => {
-        if (visited[x][y] === 1) return
+        if (grid[x][y] === 1) return
 
-        visited[x][y] = 1
+        grid[x][y] = 0
 
         if (grid[x + 1]?.[y] && parseInt(grid[x + 1][y]) === 1) {
             markIsland(x + 1, y)
@@ -46,7 +34,7 @@ var numIslands = function(grid) {
         for (let [yIdx, val] of row.entries()) {
             val = parseInt(val)
             
-            if (val === 1 && visited[xIdx][yIdx] === 0) {
+            if (val === 1) {
                 markIsland(xIdx, yIdx)
                 output++
             }
